0febefbdbc41322abf8943867c78e6f1fd3c3737
FLIGHEN <n3806@yandex.ru>
2026-05-11 22:23:46 +0300

    fix

--- src/daemon.c
+++ src/daemon.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -14,7 +15,7 @@ int daemonize(void)
     pid_t pid = fork();
 
     if (pid < 0) {
-        perror("fork failed");
+        perror("first fork failed");
         return -1;
     }
 
@@ -27,7 +28,37 @@ int daemonize(void)
         return -1;
     }
 
-    close(STDIN_FILENO);
+    pid = fork();
+
+    if (pid < 0) {
+        perror("second fork failed");
+        return -1;
+    }
+
+    if (pid > 0) {
+        _exit(0);
+    }
+
+    if (chdir("/") == -1) {
+        perror("chdir failed");
+        return -1;
+    }
+
+    int devnull = open("/dev/null", O_RDONLY);
+
+    if (devnull == -1) {
+        perror("open(/dev/null) failed");
+        return -1;
+    }
+
+    if (dup2(devnull, STDIN_FILENO) == -1) {
+        perror("dup2(stdin) failed");
+        close(devnull);
+        return -1;
+    }
+
+    close(devnull);
+
     is_daemon = 1;
 
     return 0;
